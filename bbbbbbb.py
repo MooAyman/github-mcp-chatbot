317cc66145a022fbd4cb4985dec0c37473f1a4f3
Mohamed Ayman <mohamed.ayman.ae@gmail.com>
2026-09-08 15:15:04 +0300

    Add test file for testing Automated Doc Repo project

--- bbbbbbb.py
+++
@@ -1,9 +0,0 @@
-"""Print the tools advertised by the GitHub MCP Server."""
-
-from
-    try:
-        await client.connect()
-        tools = await client.list_tools()
-        for tool in tools:
-            descri
-    asyncio.run(main())
